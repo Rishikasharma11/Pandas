ea8ec49ebc38ba593d2cae392bba25f25cffacb1
Rishika Sharma <rishikaosharma@gmail.com>
2024-09-11 13:20:39 +0530

    Update DataFrames.py

--- DataFrames.py
+++ DataFrames.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+x = ['Virat', 'Rohit', 'Hardik', 'Jadeja']
+df = pd.DataFrame(x)
+print(df)
+
 data = {
     "Name" : ["Rishika", "Shruti", "Kirti"],
     "Age" : [22, 20, 18],
@@ -8,3 +12,5 @@ data = {
 
 df = pd.DataFrame(data)
 print(df)
+
+

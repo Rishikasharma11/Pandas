b0cb425463f9cb1bca18089f397f0c58b7499596
Rishika Sharma <rishikaosharma@gmail.com>
2024-09-11 13:21:09 +0530

    Create Series.py

---
+++ Series.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+info = np.array(['P', 'A', 'N', 'D', 'A', 'S'])
+x = pd.Series(info)
+print(x)
+
+game = np.array(['C','R', 'I', 'C', 'K', 'E', 'T'])
+x = pd.Series(game)
+print(x)

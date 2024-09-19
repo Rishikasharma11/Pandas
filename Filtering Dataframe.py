434ea172c1743090fe311df6cd6bc6b01cde1661
Rishika Sharma <rishikaosharma@gmail.com>
2024-09-19 15:19:19 +0530

    Create Filtering Dataframe.py

---
+++ Filtering Dataframe.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = pd.read_csv("nba.csv")
+print(data)
+
+df = data['Team'] == 'Boston Celtics'   
+print(df)    #this is a boolean series because the output will be either true or false
+print(type(df))
+
+mask = data['Team'] == 'Boston Celtics'
+print(data[mask])
+print(data[mask].shape[0])
+
+def team_count(team):
+    mask = data['Team'] == team
+    return data[mask].shape[0]  
+
+team_count('Boston Celtics')
